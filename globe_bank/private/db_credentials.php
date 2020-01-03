17080e48f9aa284259de2a3c12155039af9b479f
OgnjenOC <ocicovic94@gmail.com>
2020-01-03 17:43:54 -0500

    Second

--- globe_bank/private/db_credentials.php
+++ globe_bank/private/db_credentials.php
@@ -2,8 +2,8 @@
 
 //define constant values 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
+define("DB_USER", "cile");
+define("DB_PASS", "somepass");
 define("DB_NAME", "globe_bank");
 ?>
 
