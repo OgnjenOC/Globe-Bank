125de08000963350b3fe1f2e942b375f52b6d9ed
OgnjenOC <ocicovic94@gmail.com>
2020-01-08 15:49:04 -0500

    collapsing navigation #6

--- globe_bank/private/shared/public_navigation.php
+++ globe_bank/private/shared/public_navigation.php
@@ -11,17 +11,18 @@
     //returns all the subjects from a database
     $nav_subjects = find_all_subjects(); 
     
-
     ?>
-    
     <ul class="subjects">
-      <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) {?>
-      <li class="<?php if($nav_subject['id'] == $subject_id){echo 'selected';} ?>">
-            <a href="<?php echo url_for('index.php'); ?>">
+      <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
+      <li class="<?php if($nav_subject['id'] == $subject_id){
+                  echo 'selected';} ?>">
+          <a href="<?php echo url_for('index.php?subject_id=' . 
+                  h(u($nav_subject['id']))); ?>">
               <?php echo h($nav_subject['menu_name']); 
               ?>
             </a>
                 
+   <?php if($nav_subject['id'] == $subject_id){ ?>
                 <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
                 <ul class="pages">
                   <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) {?>
@@ -34,11 +35,13 @@
         
                 <?php } // while $nav_pages ?>
                 </ul>
-            
+                <?php mysqli_free_result($nav_pages); ?>
+                <?php } //if($nav_subject['id'] == $subject_id ?>
       </li>
         
        <?php } // while $nav_subjects ?>
       
     </ul>
     <?php mysqli_free_result($nav_subjects); ?>
+   
 </navigation>
